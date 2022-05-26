de4ece12753140354eed7dc3c51f377fecffe6f6
NivethaGopi <105581864+NivethaGopi@users.noreply.github.com>
2022-05-26 18:51:05 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Calculator {
+	public int getSum(int num1,int num2, int num3)
+	{
+		int sum = num1 + num2 +num3;
+		return sum;
+	}
+	public int getDifference(int num1,int num2)
+	{
+		int Subtraction = num1 - num2;
+		return Subtraction;
+	}
+	public double getMultiple(double num1,double num2)
+	{
+		double Product = num1 * num2;
+		return Product;
+	}
+	public float getDivision(float num1,float num2)
+	{
+		float Division = num1 / num2;
+		return Division;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal = new Calculator();
+		System.out.println(cal.getSum(5, 10, 15));
+		System.out.println(cal.getDifference(50, 22));
+		System.out.println(cal.getMultiple(156.3, 22.8));
+		System.out.println(cal.getDivision(82.1f, 40.3f));
+
+	}
+
+}
